b02ca2d1f7f8291f6a100a2911c2e79798628942
pirate <jorge.egea@ikasle.egibide.org>
2023-09-28 09:42:15 +0200

    Refactored

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const url = "url a nuestro plc";
 
-const start = document.getElementById("start");
+const startButton = document.getElementById("start");
 let isOn = false;
-fetch(url)
-    .then(response => {
-        if (response.ok)
-            return response.json()
-        else
-            throw new Error("No se ha podido hacer fetch["+url+"].")
-    })
-    .then(data => {
-        isOn = data;
-    })
-    .catch(error => {
-        console.error("fetch error["+url+"]: " + error)
-    });
+
+startButton.addEventListener("click", () => {
+    fetch(url)
+        .then(response => {
+            if (response.ok)
+                return response.json()
+            else
+                throw new Error("No se ha podido hacer fetch["+url+"].")
+        })
+        .then(data => {
+            isOn = data;
+        })
+        .catch(error => {
+            console.error("fetch error["+url+"]: " + error)
+        });
+});
