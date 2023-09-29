91636b9a7d028fb6ecb3874afd824fb9201cd926
pirate <theunknownsainth@outlook.com>
2023-09-29 11:00:01 +0200

    Train needs work

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+const root = document.documentElement;
 const trainMode = document.querySelector("#trainMode");
 const destinoWrapper = document.querySelector(".destino-wrapper");
 const terminarCiclo = document.querySelector(".terminarCiclo");
 const train = document.querySelector("#train");
 const start = document.querySelector("#start");
 const stop = document.querySelector("#stop");
+const finishCycle = document.querySelector(".terminarCiclo");
 // start
 let pm;
 let pausa;
@@ -17,6 +19,9 @@ let p3;
 let p4;
 let p5;
 
+let trainStart = 20;
+let trainEnd = 100;
+
 /*Start*/
 trainMode.addEventListener("click", () => {
     if (trainMode.checked) {
@@ -29,19 +34,50 @@ trainMode.addEventListener("click", () => {
     }
 });
 
-start.addEventListener("click", () => {
-    if (trainMode.checked){
-        train.style.animationName = "train-animation";
+let animations = ["st1", "st2", "st3", "st4", "st1r", "st2r", "st3r", "st4r"];
+let positions = ["20%", "40%", "60%", "80%", "100%"];
+let currentIndex = 0;
+let animationTerminate = false;
+const selectElement = document.getElementById("destino");
 
-    }else {
-        /*Animacion de momdo manual*/
+
+start.addEventListener("click", () => {
+    animationTerminate = false;
+    if (trainMode.checked) {
+        playNextAnimation(animations, currentIndex);
+    } else {
+        let selectedIndex = selectElement.selectedIndex;
+        root.style.setProperty('--train-end', positions[selectedIndex]);
+        train.style.animation = "0.5s linear 0s 1 forwards manual";
     }
-})
+});
+
+
 
 /*Stop*/
 stop.addEventListener("click", () => {
-    train.style.animation = "none";
-
-    const trainPosition = getComputedStyle(train).left;
-    train.style.left = trainPosition;
+    const computedStyle = window.getComputedStyle(train).getPropertyValue("left");
+    train.removeEventListener("animationend", onAnimationEndOnce);
+    train.style.animation = "2s linear 1s 1 both reset";
+    currentIndex = 0;
+    train.style.left = computedStyle;
 })
+
+finishCycle.addEventListener("click", () => {
+    animationTerminate = true;
+});
+
+function playNextAnimation(array, index) {
+    console.log(animations[index]);
+    train.style.animation = "0.5s linear 0.25s 1 both " + array[index];
+    train.addEventListener("animationend", onAnimationEndOnce);
+}
+
+function onAnimationEndOnce() {
+    currentIndex++
+    if (currentIndex >= animations.length)
+        currentIndex = 0;
+    if (!(animationTerminate && currentIndex == 0)) {
+        playNextAnimation(animations, currentIndex);
+    }
+}
