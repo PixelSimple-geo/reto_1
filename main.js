63df427af49bc21261b4523f2fa2f2b09f1f6704
pirate <jorge.egea@ikasle.egibide.org>
2023-10-03 08:48:45 +0200

    Finished cycle done

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const train = document.querySelector(".train");
 const finishCycle = document.querySelector(".terminarCiclo");
 const mode = document.querySelector("#trainMode");
 
-let positions = ["20%", "40%", "60%", "80%", "100%",];
+let positions = ["0%", "20%", "40%", "60%", "80%", "100%",];
 const cycle = ["20%", "40%", "60%", "80%", "100%", "80%", "60%", "40%", "20%"];
 let currentPosition = 0;
 let animationInterval;
@@ -29,22 +29,24 @@ start.addEventListener("click", () => {
         start.disabled = true;
         finishCycle.disabled = false;
         animationInterval = setInterval(() => {
-            train.style.transition = "all 350ms";
-            playNextAnimation(cycle, currentPosition);
+            if (terminateCycle && currentPosition === 0) {
+                clearInterval(animationInterval);
+                train.style.left = positions[currentPosition];
+                train.style.transition = "all 500ms";
+                terminateCycle = false;
+            } else {
+                train.style.transition = "all 350ms";
+                playNextAnimation(cycle, currentPosition);
+            }
             currentPosition++;
             if (currentPosition >= cycle.length) {
                 currentPosition = 0;
-            }
-            if (terminateCycle && currentPosition === 0) {
-                clearInterval(animationInterval);
-                train.style.left = cycle[currentPosition];
-                train.style.transition = "all 350ms";
-                terminateCycle = false;
             }
         }, 350);
     } else {
         const selectElement = document.querySelector('select');
         const selectedIndex = selectElement.selectedIndex;
+        train.style.transition = "all 500ms";
         train.style.left = positions[selectedIndex];
     }
 });
@@ -60,14 +62,6 @@ stopButton.addEventListener("click", () => {
 finishCycle.addEventListener("click", () => {
     terminateCycle = true;
     start.disabled = false;
-    if (currentPosition == 1){
-
-    }
-    setTimeout(() => {
-        train.style.left = "0%";
-        currentPosition = 0;
-        train.style.transition = "all 2000ms";
-    }, 3000);
 });
 
 
