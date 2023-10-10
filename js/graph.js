b3af60c36332cfb04d4db929140507a0b358cdc7
pirate <jorge.egea@ikasle.egibide.org>
2023-10-10 09:04:07 +0200

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	js/main.js

--- js/graph.js
+++ js/graph.js
@@ -4,18 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const labels = ["P1", "P2", "P3", "P4", "P5"];
 
     function updateChart() {
-        var data = [0, 0, 0, 0, 0];
+        let data = [0, 0, 0, 0, 0];
 
         // Retrieve the stopCounts data from localStorage
-        var stopCounts = JSON.parse(localStorage.getItem("stopCounts")) || {};
+        let stopCounts = JSON.parse(localStorage.getItem("stopCounts")) || {};
 
-        for (var i = 0; i < data.length; i++) {
-            var location = positions[i];
+        for (let i = 0; i < data.length; i++) {
+            let location = positions[i];
             data[i] = stopCounts[location] || 0;
         }
 
         // Create or update the chart
-        var chart = new Chart(grafico, {
+        let chart = new Chart(grafico, {
             type: "bar",
             data: {
                 labels: labels,
